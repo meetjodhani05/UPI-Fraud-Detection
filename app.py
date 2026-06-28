25bf57119ed5e88ecb8d9a6c9d533b609814f84a
Meet Jodhani <meetjodhani05@gmail.com>
2026-06-28 10:03:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("UPI Fraud Detection Project")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>UPI Fraud Detection</h1>
+    <p>Welcome to the UPI Fraud Detection Project.</p>
+    <p>Project created using Python and Flask.</p>
+    """
+
+if __name__ == "__main__":
+    app.run(debug=True)
